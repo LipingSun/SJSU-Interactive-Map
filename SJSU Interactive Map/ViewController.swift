188a87ac604058e763b7e3df1736bff27556a7dc
LipingSun <liping.sun@sjsu.edu>
2015-11-10 11:03:22 -0800

    Initial Commit

---
+++ SJSU Interactive Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SJSU Interactive Map
+//
+//  Created by Liping on 11/10/15.
+//  Copyright © 2015 Liping. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
