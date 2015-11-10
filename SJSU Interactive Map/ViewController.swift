f5ad7634fa557bd8ccda82747230afdad664952c
LipingSun <liping.sun@sjsu.edu>
2015-11-10 12:10:44 -0800

    setup viewcontroller

--- SJSU Interactive Map/ViewController.swift
+++ SJSU Interactive Map/ViewController.swift
@@ -8,16 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var btn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView.image = UIImage(named: "campusmap")
+        scrollView.contentSize = imageView.image!.size
+        scrollView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let imageViewSize = imageView.bounds.size
+        let scrollViewSize = scrollView.bounds.size
+        
+//        print(imageViewSize.height, imageViewSize.width, scrollViewSize.height, scrollViewSize.width)
+        
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        
+        let scale = min(widthScale, heightScale)
+        
+        scrollView.zoomScale = scale
+        scrollView.minimumZoomScale = scale
+        scrollView.maximumZoomScale = 2
+        
+        //        print(imageViewSize.height * scale, imageViewSize.width * scale)
+        
+        let verticalPadding = imageViewSize.height * scale < scrollViewSize.height ?
+            (scrollViewSize.height - imageViewSize.height * scale) / 2 : 0
+        let horizontalPadding = imageViewSize.width * scale < scrollViewSize.width ?
+            (scrollViewSize.width - imageViewSize.width * scale) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding,
+            bottom: verticalPadding, right: horizontalPadding)
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 
 
