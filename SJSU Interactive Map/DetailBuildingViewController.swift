2263a9a53df33e8a9378fd05315a531c9a5efe36
LipingSun <liping.sun@sjsu.edu>
2015-11-13 00:30:25 -0800

    implement back button functionality

--- SJSU Interactive Map/DetailBuildingViewController.swift
+++ SJSU Interactive Map/DetailBuildingViewController.swift
@@ -22,21 +22,21 @@ class DetailBuildingViewController: UIViewController {
     @IBOutlet weak var time: UILabel!
     @IBOutlet weak var distance: UILabel!
     @IBOutlet weak var image: UIImageView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         distance.textColor=UIColor.blueColor()
         //distance.shadowColor = UIColor.blueColor()
         time.textColor=UIColor.blueColor()
         //time.shadowColor = UIColor.blueColor()
-
+        
         
         
         buildingName.font = UIFont(name: "HelveticaNeue", size: CGFloat(22))
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -55,20 +55,24 @@ class DetailBuildingViewController: UIViewController {
         image.image = photoImage
     }
     
+    @IBAction func backBtnClick() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
