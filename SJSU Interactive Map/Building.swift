1cd2cfd32351a5d35e7daf05152cb25374f7fc05
LipingSun <liping.sun@sjsu.edu>
2015-11-13 00:17:56 -0800

    Building model

---
+++ SJSU Interactive Map/Building.swift
@@ -0,0 +1,34 @@
+//
+//  Building.swift
+//  SJSUMap
+//
+//  Created by xiaoxiao li on 11/9/15.
+//  Copyright © 2015 xiaoxiao li. All rights reserved.
+//
+
+import UIKit
+
+class Building {
+    //MARK: Properties
+    
+    var name: String
+    var address: String
+    var photo: UIImage?
+    var lat: Double
+    var lng: Double
+    var time: String?
+    var distance: String?
+    var button: UIButton?
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, address: String, lat: Double, lng: Double) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.address = address
+        self.lat = lat
+        self.lng = lng
+    }
+    
+}
